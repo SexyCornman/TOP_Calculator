ec297b2aceaec8a81f2c96fb4391d36f8cb2bb45
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-10-17 13:36:05 +0700

    add basic functions

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function add() {
+   result = Number(currentOperand.innerText)+Number(previousOperand.innerText);
+   return result
+}
+
+function subtract(){
+    result = Number(currentOperand.innerText) - Number(previousOperand.innerText);
+   return result
+
+}
+
+function multiply(){
+    result = Number(currentOperand.innerText) * Number(previousOperand.innerText);
+   return result
+
+}
+
+function divide(){
+    result = Number(currentOperand.innerText) / Number(previousOperand.innerText);
+   return result
+
+}
+
+function clear(){
+    currentOperand.innerText = "0";
+    previousOperand.innerText ="";
+}
+
+function operation(){
+
+
+}
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const clearButtons = document.querySelector('[data-clear]');
