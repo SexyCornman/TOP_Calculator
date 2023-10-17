7f6668612854808459bf8d28a013e98fad84a158
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-10-17 12:59:39 +0700

    declared elements in javascript

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const clearButtons = document.querySelector('[data-clear]');
+const deleteButtons = document.querySelector('[data-delete]');
+const equalButtons = document.querySelector('[data-equal]');
+const previousOperand = document.querySelector('[data-previous-operand]');
+const currentOperand = document.querySelector('[data-current-operand]');
 
